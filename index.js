aa4b9189cc87769e7b314911f85dc7d68805e108
Emanuel <emanuel@startapp.one>
2022-01-25 23:03:27 -0300

    time difference in progress

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 const dateInput = document.getElementById("dateInput");
+const resultDiv = document.getElementById("test");
 
 const submit = () => {
-    console.log("opa");
+    const dateNow = new Date().getTime();
+    const dateSelected = new Date(dateInput.value).getTime()
+    const diff = dateSelected - dateNow;
+
+    const miliseconds = (diff / 1000);
+    const seconds = (miliseconds % 60);
+    console.log("segundos: ",seconds)
+    
+    const minutes = ((miliseconds/60) % 60);
+    console.log("minutos: ",minutes)
+    const hours = (((miliseconds/(60*60))) % 24);
+    console.log("hours: ", Math.round(hours));
+    resultDiv.innerHTML = `${Math.trunc(hours)}:${Math.trunc(minutes)}:${Math.trunc(seconds)}`;
     setTimeout(()=>submit(), 1000);
 }
